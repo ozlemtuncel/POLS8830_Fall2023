b05e4f8f3a0858d9880bf5a796411e7936d2b0a8
Ozlem Tuncel <46090556+ozlemtuncel@users.noreply.github.com>
2023-09-12 21:05:44 -0400

    update week 4

--- docs/Week4_Logit_Probit_Models_tidy.R
+++ docs/Week4_Logit_Probit_Models_tidy.R
@@ -48,12 +48,31 @@ women_data$lfp <- as.numeric(women_data$lfp)
 women_data$lfp <- replace(women_data$lfp, women_data$lfp == 1, 0)
 women_data$lfp <- replace(women_data$lfp, women_data$lfp == 2, 1)
 women_data$lfp <- as.factor(women_data$lfp)
+attributes(women_data$lfp)
 summary(women_data$lfp)
 
-# Can easily do this yourself, but checking that the 0,1 ratio isn't above 60/40 or 40/60
+# This is what Russ prefers 
+
+# But I always use "ifelse"
+women_data$lfp_new <- ifelse(women_data$lfp == "no", 0, 1)
+women_data$lfp_new <- factor(women_data$lfp_new)
+
+# Can easily do this yourself 
+# but checking that the 0, 1 ratio isn't above 60/40 or 40/60
 CrossTable(women_data$lfp)
 
-attach(women_data)
+# or alternatively
+prop.table(table(women_data$lfp))*100
+
+# I would also do EDA 
+plot(lfp~., data = women_data)
+
+# Or ggplot version
+women_data |> 
+  gather(key = "variable", value = "value", -lfp) |> 
+  ggplot(aes(x = lfp, y = value)) +
+  geom_col() +
+  facet_wrap(~variable)
 
 ### Logit and probit models -----
 
@@ -85,7 +104,9 @@ stargazer(m1, m2, m3, m4, type = "text")
 
 ### Proportional Reduction in Error ----
 # Russ previously created his own function (see Wald Stargazer and PRE scripts)
-# DAmisc package has pre() function that helps us with this
+# DAMisc package has pre() function that helps us with this
+
+# what degree knowing a variable x can help you to predict another variable y
 pre_m1 <- pre(m1)
 pre_m2 <- pre(m2)
 pre_m3 <- pre(m3)
@@ -98,21 +119,23 @@ wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 2:length(m2$coefficients))
 wald.test(b = coef(m3), Sigma = vcov(m3), Terms = 2:length(m3$coefficients))
 wald.test(b = coef(m4), Sigma = vcov(m4), Terms = 2:length(m4$coefficients))
 
+# Here we use generic vcov but we can change this depending on our vcov
+
 # To get this into your output table you can either manually copy and paste the 
 # values into your table, or we can use some user generated functions to create 
 # a function that does this for you
 
 # To call the correct number of stars from the p value of the wald test
-wald.test.stars<- function(pvalue){
+wald.test.stars <- function(pvalue){
   if(pvalue < 0.1 & pvalue >= 0.05){return("*")
     } else if(pvalue < 0.05 & pvalue >= 0.01){return("**")
     } else if(pvalue < 0.01){return("***")
     } else {return(" ")}
 }
 
+
 # Testing
 wald.test.stars(0.001)
-wald.test.stars(0.01)
 wald.test.stars(0.02)
 wald.test.stars(0.06)
 wald.test.stars(0.11)
@@ -120,9 +143,12 @@ wald.test.stars(0.11)
 # Pulling the chi2 statistic from the wald.test
 stargazer.wald.chi <- function(model){
   require(aod)
+  
   w1 <- wald.test(b = coef(model), Sigma = vcov(model), Terms = 2:length(model$coefficients))
+  
   w1chi <- w1$result$chi2[1]
-    return(format(round(w1chi, 3), nsmall = 3)) 
+  
+  return(format(round(w1chi, 3), nsmall = 3)) 
   }
 
 # Testing
@@ -130,10 +156,15 @@ stargazer.wald.chi(m1)
 
 # Pulling the significance level from the wald.test 
 stargazer.wald.sig<- function(model){
+  
   require(aod)
-  w1<-wald.test(b = coef(model), Sigma = vcov(model), Terms = 2:length(model$coefficients))
-  w1p<-w1$result$chi2[3]
-  starw1<-wald.test.stars(w1p)
+  
+  w1 <- wald.test(b = coef(model), Sigma = vcov(model), Terms = 2:length(model$coefficients))
+  
+  w1p <- w1$result$chi2[3]
+  
+  starw1 <- wald.test.stars(w1p)
+  
   return(starw1)
 }
 
@@ -142,7 +173,9 @@ stargazer.wald.sig(m1)
 
 # Putting everything together for stargazer
 stargazer.wald.output <- function(model){
-  out<-paste(stargazer.wald.chi(model), stargazer.wald.sig(model))
+  
+  out <- paste(stargazer.wald.chi(model), stargazer.wald.sig(model))
+  
   return(out)
 }
 
@@ -177,7 +210,7 @@ stargazer(m1, m2, m3, m4,
 
 ### AIC and BIC ----
 AIC(m1)
-BIC(m1)
+AIC(m2)
 
 m1
 m2
@@ -186,18 +219,30 @@ BIC(m1)
 BIC(m2)
 
 ### Likelihood Ratio Test (LR) ----
+
+# lrtest(fullmodel, reducedmodel) => this is how you test it
+
 lrtest(m2, m1)
+
+# p<0.5 => This indicates that the full model and the nested model do not fit 
+# the data equally well. As a result, we should employ the entire model.
 lrtest(m4, m3)
 
 m3$coefficients
 m4$coefficients
 
-lrtest(m4, m3)
-
 ### Wald Test for Model Comparison ----
 # Wald Test (from aod package)
 
 summary(m2)
+
+# Here you can test specific variables with Wald test
+# testing whether my variables add something to the model
+
+# If the test rejects the null hypothesis, this suggests that the variables 
+# are significant to that model fit. If the test results could not reject the 
+# null hypothesis, this means that removing the variables from the model will 
+# not considerably damage the fit of that model.
 
 wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 2:2)
 wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 3:3)
@@ -205,3 +250,58 @@ wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 4:4)
 wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 5:5)
 wald.test(b = coef(m2), Sigma = vcov(m2), Terms = 2:3)
 
+### Rare Events ----
+library(Zelig)
+
+# This was our model
+m1 <- glm(lfp ~ k5 + k618, 
+          family = binomial(link = "logit"),
+          data = women_data)
+summary(m1)
+
+# Normally, we need to make sure that we do not have any NA in data. 
+# This is a toy data that has zero NAs. So, we are lucky!
+
+# Perform model
+rare_mod1 <- zelig(as.numeric(as.character(lfp)) ~ k5 + k618,  
+                   data = women_data, 
+                   model = "relogit")
+
+summary(rare_mod1)
+
+### Penalized MLE or Firth ----
+library(logistf)
+
+# Firth's bias-Reduced penalized-likelihood logistic regression
+firth_mod1 <- logistf(lfp ~ k5 + k618, 
+                      data = women_data, 
+                      firth = T)
+
+summary(firth_mod1)
+
+### Let's compare logit, probit, rare, and panelized MLE
+stargazer(m1, m3, rare_mod1, firth_mod1) # this won't work!!!
+
+my_models <- list(m1, m3, from_zelig_model(rare_mod1), firth_mod1)
+lapply(my_models, summary)
+
+# Combine models
+my_models <- list(m1, m3, from_zelig_model(rare_mod1), firth_mod1)
+
+# Plot these models
+dwplot(my_models, 
+       dot_args = list(aes(colour = model, shape = model)), 
+       size = 5,
+       ci = 0.95) |> 
+  relabel_predictors(c(k5 = "Number of kids under 5", 
+                       k618 = "Number of kids aged 6-18")) +
+  geom_vline(xintercept = 0) +
+  theme_bw() +
+  theme(plot.title = element_text(face="bold"),
+        legend.background = element_rect(colour="grey80"),
+        legend.title.align = .5) +
+  labs(x = "Log-odds (coefficients) with 95% CIs") +
+  scale_color_discrete(name = "Models", 
+                       labels = c("Logit", "Probit", "Rare", "Firth")) +
+  scale_shape_discrete(name = "Models", 
+                       labels = c("Logit", "Probit", "Rare", "Firth"))
